53fc4499bfde8beafb79610f7761c4a63466ab9c
moldic <moldic35@gmail.com>
2022-09-02 11:06:31 +0900

    sample作成

--- app/1.py
+++ app/1.py
@@ -1,3 +1,27 @@
 import PySimpleGUI as sg
+import pyperclip3 as pc3
 
-sg.popup('test')
+layout=[
+
+    [sg.Input(),sg.FileBrowse()],
+    [sg.Button('test',key='-MainBtn-')],
+    [sg.Text('',key='-Out1-')],
+    [sg.Input(key='-Input-',enable_events=True)]
+    
+]
+for i in range(3):
+    layout+= [[sg.Text('aaa')],[sg.Button('',key='-MainBtn-'+str(i))]]
+
+window=sg.Window('タイトル',layout,size=(600,300),finalize=True)
+
+while True:
+    event,values=window.read()
+    if event=='-MainBtn-':
+        pc3.copy(str(window['-Input-'].get()))
+        sg.popup('copied')
+    if event =='-Input-':
+        window['-Out1-'].Update(str(values['-Input-']).upper())
+    if event==sg.WIN_CLOSED or event=='Exit':
+        break
+
+window.close()
