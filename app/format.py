80feab0519e1891e93b8afa49b9b8c24aced05b0
moldic <moldic35@gmail.com>
2022-09-02 11:06:58 +0900

    lexer自作チャレンジ開始

--- app/format.py
+++ app/format.py
@@ -5,9 +5,40 @@ class Token:
 
     def __init__(self,str):
         self.str=str
+        self.tokens=[]
 
+    def add(self,Token):
+        self.tokens.add(Token)
 
-class Format:
+    #dummy
+    def next(self):
+        return ''
 
+class InputText:
     def __init__(self,str,dl):
         self.tokens=list(map(lambda x:Token(x),str.split(dl)))
+
+#Cannot-Recursive
+#str:row-string
+class Format:
+    def lex(self,str,tokens=[]):
+        while len(str)!=0:
+            if (i:=str.find('{'))!=-1:
+                tokens.append(str[:i])
+                str=str[i+1:]
+                print(str)
+                break
+            else:
+                tokens.append(str)
+                break
+        return tokens
+
+    def __init__(self,str):
+        #validator
+        if str.count('{')!= str.count('}'):
+            return
+        #token
+        self.lex(str)
+
+
+Format('hoge{h}oge')
